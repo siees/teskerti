7898f4e6782e233ee2bccb9d8e1b33553e7c94d4
Nouris <medanouar.hrairi@gmail.com>
2022-04-29 10:47:28 +0100

    auth service entry point init

---
+++ auth/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import { currentUserRouter } from './routes/current-user';
+
+const app = express();
+app.use(express.json());
+
+app.use(currentUserRouter);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!!!');
+});
