5b8366ee81b7e4d8fc152a0b4b6165f7c3c75dd8
Nouris <medanouar.hrairi@gmail.com>
2022-05-05 16:26:18 +0100

    app.ts setup with create new ticket router

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import 'express-async-errors';
-import { errorHandler, NotFoundError } from '@teskerti/common';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@teskerti/common';
+
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all('*', () => {
   throw new NotFoundError();
