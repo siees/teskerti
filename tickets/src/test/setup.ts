b7dff2b732ebd1d938df1e4378bc6b173a5eaf37
Nouris <medanouar.hrairi@gmail.com>
2022-05-05 16:25:45 +0100

    created fake cookie for testing

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,10 +1,9 @@
-import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  function signup(): Promise<string[]>;
+  function signup(): string[];
 }
 
 let mongo: any;
@@ -31,16 +30,22 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signup = async (): Promise<string[]> => {
-  const email = 'test@test.com';
-  const password = 'test';
+global.signup = () => {
+  //build a JWT payload
+  const payload = { id: '1', email: 'test@test.com' };
 
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({ email, password })
-    .expect(201);
+  //Create a JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  const cookie = response.get('Set-Cookie');
+  //Build session Object {JWT : myJWT}
+  const session = { jwt: token };
 
-  return cookie;
+  //Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  //take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  //return a String thats the cookie with the encoded data
+  return [`session=${base64}`];
 };
