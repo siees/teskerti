ed41c03139cb1b7ea3b3051c9b409ea652fdd9f9
Nouris <medanouar.hrairi@gmail.com>
2022-05-05 13:24:27 +0100

    updated MONGO_URI call

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -3,14 +3,14 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT key must be defined');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
   try {
-    if (!process.env.JWT_KEY) {
-      throw new Error('JWT key must be defined');
-    }
-
-    await mongoose.connect(
-      'mongodb://tickets-mongo-clusterip-srv:27017/tickets'
-    );
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log('Connected to MongoDb');
   } catch (err) {
