cbc815c3e4f460cad5d44ac88ded24f13f6e7feb
Nouris <medanouar.hrairi@gmail.com>
2022-05-05 17:27:03 +0100

    added router endpoints

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,8 @@ import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@teskerti/common';
 
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -17,6 +19,8 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all('*', () => {
   throw new NotFoundError();
