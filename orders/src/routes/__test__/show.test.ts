ba899f75b57765e52273a61d5f5151b72cb7036c
Nouris <medanouar.hrairi@gmail.com>
2022-05-07 15:45:28 +0100

    show order route handler logic and tests

---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,61 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+it('returns a 404 if a order is not found', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .get(`/api/orders/${id}`)
+    .set('Cookie', global.signup())
+    .send()
+    .expect(404);
+});
+
+it('returns a 401 if user tries to fetch other users orders', async () => {
+  const ticket = Ticket.build({
+    title: 'title',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signup();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', global.signup())
+    .send()
+    .expect(401);
+});
+
+it('returns the order if the order is found', async () => {
+  const ticket = Ticket.build({
+    title: 'title',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signup();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  const { body: fetchOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  expect(fetchOrder.id).toEqual(order.id);
+});
