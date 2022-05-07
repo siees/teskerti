380882e3722c6e1b75f85772099e646bb4d3ee14
Nouris <medanouar.hrairi@gmail.com>
2022-05-07 15:45:06 +0100

    create order route handler logic and tests

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,80 @@
+import request from 'supertest';
+import mongoose from 'mongoose';
+
+import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
+import { natsWrapper } from '../../nats-wrapper';
+
+it('has a route handler listening to /api/orders for post request', async () => {
+  const response = await request(app).post('/api/orders').send({});
+
+  expect(response.status).not.toEqual(404);
+});
+
+it('can only be accessed if the user is signed in', async () => {
+  await request(app).post('/api/orders').send({}).expect(401);
+});
+
+it('returns status other than 401 if user is signed in', async () => {
+  const response = await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signup())
+    .send({});
+
+  expect(response.status).not.toEqual(401);
+});
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signup())
+    .send({ ticketId })
+    .expect(404);
+});
+
+it('returns an error if the ticket is already reserved', async () => {
+  const ticket = Ticket.build({
+    title: 'title',
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: 'userId',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signup())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it('reserves a ticket', async () => {
+  let orders = await Order.find({});
+  expect(orders.length).toEqual(0);
+
+  const ticket = Ticket.build({
+    title: 'title',
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signup())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  orders = await Order.find({});
+  expect(orders[0].status).toEqual(OrderStatus.Created);
+});
+
+it.todo('publishes an event');
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,64 @@
 import express, { Request, Response } from 'express';
+import {
+  BadRequestError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from '@teskerti/common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
+
+import { Ticket } from '../models/ticket';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+// The number of seconds that the user has to pay for the order
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input)) //check if id is mongoose ObjectId type
+      .withMessage('Ticket Id must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    // Build the order and save it to the DB
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      expiresAt: expiration,
+      status: OrderStatus.Created,
+      ticket,
+    });
+    await order.save();
+
+    // Publish an event that an order was created
+
+    res.status(201).send(order);
+  }
+);
 
 export { router as createOrderRouter };
