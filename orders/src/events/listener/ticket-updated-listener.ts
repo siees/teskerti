6d80d46d048b88b0b2b4854574793b0eee95223c
Nouris <medanouar.hrairi@gmail.com>
2022-05-07 18:56:28 +0100

    init order service event listeners

---
+++ orders/src/events/listener/ticket-updated-listener.ts
@@ -0,0 +1,25 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, TicketUpdatedEvent } from '@teskerti/common';
+
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName: string = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const { id, title, price } = data;
+
+    const ticket = await Ticket.findById(id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
