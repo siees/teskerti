e45750d44fd4c0b51492a0294e44036978f9b62f
Nouris <medanouar.hrairi@gmail.com>
2022-05-07 16:05:30 +0100

    cancel order route handler logic and tests

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,54 @@
+import request from 'supertest';
+
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { OrderStatus } from '../../models/order';
+
+it('has a route handler listening to /api/orders/:orderId for delete request', async () => {
+  const response = await request(app).delete('/api/orders/test').send({});
+
+  expect(response.status).not.toEqual(404);
+});
+
+it('can only be accessed if the user is signed in', async () => {
+  await request(app).delete('/api/orders/test').send({}).expect(401);
+});
+
+it('returns status other than 401 if user is signed in', async () => {
+  const response = await request(app)
+    .delete('/api/orders/test')
+    .set('Cookie', global.signup())
+    .send({});
+
+  expect(response.status).not.toEqual(401);
+});
+
+it('marks an order as cancelled', async () => {
+  const ticket = Ticket.build({
+    title: 'title',
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signup();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+  const { body: fetchOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  expect(fetchOrder.status).toEqual(OrderStatus.Cancelled);
+});
