0b7efee7cf1303c8fcbac373804d2d9cc7b58268
Nouris <medanouar.hrairi@gmail.com>
2022-05-09 00:45:45 +0100

    imported and init listeners

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
+import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -15,6 +17,9 @@ const start = async () => {
   }
   if (!process.env.NATS_URI) {
     throw new Error('NATS_URI must be defined');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
   }
   try {
     await natsWrapper.connect(
@@ -30,19 +35,14 @@ const start = async () => {
 
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDb');
   } catch (error) {
     console.log(error);
-  }
-
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
-  }
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log('Connected to MongoDb');
-  } catch (err) {
-    console.log(err);
   }
 
   app.listen(3000, () => {
