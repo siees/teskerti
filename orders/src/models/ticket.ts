f4a7d70e4abc4def7d45bc649fcd3bff838f0c4c
Nouris <medanouar.hrairi@gmail.com>
2022-05-08 03:01:42 +0100

    added findByEvent method

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -15,6 +15,11 @@ interface TicketAttrs {
 //that a Ticket Model has
 interface TicketModel extends Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 //interface that describes the properties
@@ -48,6 +53,10 @@ ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({ _id: attrs.id, title: attrs.title, price: attrs.price });
+};
+
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({ _id: event.id, version: event.version - 1 });
 };
 
 ticketSchema.methods.isReserved = async function () {
