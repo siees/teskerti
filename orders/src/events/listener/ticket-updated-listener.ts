77c83965d2b4a837505a807d86bbf2dd547f9c23
Nouris <medanouar.hrairi@gmail.com>
2022-05-08 02:56:01 +0100

    updated listener to handle ticket version

--- orders/src/events/listener/ticket-updated-listener.ts
+++ orders/src/events/listener/ticket-updated-listener.ts
@@ -9,9 +9,9 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   queueGroupName: string = queueGroupName;
 
   async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-    const { id, title, price } = data;
+    const { id, title, price, version } = data;
 
-    const ticket = await Ticket.findById(id);
+    const ticket = await Ticket.findOne({ _id: id, version: version - 1 });
 
     if (!ticket) {
       throw new Error('Ticket not found');
