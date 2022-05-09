84ff5c276ce179e6525b61a4ab973101d730604d
Nouris <medanouar.hrairi@gmail.com>
2022-05-09 02:29:11 +0100

    created expiration queue

---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,17 @@
+import Queue from 'bull';
+
+interface Payload {
+  orderId: string;
+}
+
+const expirationQueue = new Queue<Payload>('order:expiration', {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
+});
+
+expirationQueue.process(async (job) => {
+  console.log('want to do job');
+});
+
+export { expirationQueue };
