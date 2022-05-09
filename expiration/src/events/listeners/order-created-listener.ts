f81690d4d29dbadedbfa498764cf68def9332f16
Nouris <medanouar.hrairi@gmail.com>
2022-05-09 02:37:47 +0100

    deleted console log

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -10,7 +10,6 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
     const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
-    console.log('waiting for this many seconds', delay * 1000);
 
     await expirationQueue.add({ orderId: data.id }, { delay });
 
