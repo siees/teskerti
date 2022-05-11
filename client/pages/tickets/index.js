9bac6bc3a2b84f6e83e8dbddffaff7bbbc244a57
Nouris <medanouar.hrairi@gmail.com>
2022-05-11 15:10:33 +0100

    list tickets page

---
+++ client/pages/tickets/index.js
@@ -0,0 +1,33 @@
+const Tickets = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
+  );
+};
+
+Tickets.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
+};
+
+export default Tickets;
